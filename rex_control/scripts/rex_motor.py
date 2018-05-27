cac40d30789aa17a188f87d0445a2d1daeae2755
Guilherme A. Ribeiro <garamizo@mtu.edu>
2018-05-27 10:19:32 -0400

    update pin labels on control

--- rex_control/scripts/rex_motor.py
+++ rex_control/scripts/rex_motor.py
@@ -5,28 +5,32 @@ import RPi.GPIO as IO
 import numpy as np
 
 
-class RabbitMotor:
-    PIN_IN1 = 2
-    PIN_IN2 = 3
-    PIN_EN1 = 4
-    PIN_IN3 = 17
-    PIN_IN4 = 27
-    PIN_EN2 = 22
+class RexMotor:
+    PIN_ENA = 3
+    PIN_IN1 = 5
+    PIN_IN2 = 7
+    PIN_IN3 = 11
+    PIN_IN4 = 13
+    PIN_ENB = 15
 
     def __init__(self):
 
-        IO.setmode(IO.BCM)
+        IO.setmode(IO.BOARD)
+        IO.setup(self.PIN_ENA, IO.OUT)
         IO.setup(self.PIN_IN1, IO.OUT)
         IO.setup(self.PIN_IN2, IO.OUT)
         IO.setup(self.PIN_IN3, IO.OUT)
         IO.setup(self.PIN_IN4, IO.OUT)
-        IO.setup(self.PIN_EN1, IO.OUT)
-        IO.setup(self.PIN_EN2, IO.OUT)
+        IO.setup(self.PIN_ENB, IO.OUT)
 
-        self.en1 = IO.PWM(self.PIN_EN1, 100)
-        self.en2 = IO.PWM(self.PIN_EN2, 100)
+        self.en1 = IO.PWM(self.PIN_ENA, 100)
+        self.en2 = IO.PWM(self.PIN_ENB, 100)
+
+        IO.output(self.PIN_IN1, IO.HIGH)
+        IO.output(self.PIN_IN2, IO.LOW)
 
     def write(self, ur, ul):
+        print("%.2f %.2f" % (ur, ul))
         ur = np.clip(ur, -100, 100)
         ul = np.clip(ul, -100, 100)
 
@@ -48,8 +52,8 @@ def twist_cb(msg):
 if __name__ == "__main__":
 
     rospy.init_node('simple_nav', anonymous=True)
-    rospy.Subscriber('/rabbit_velocity_controller/cmd_vel_out', TwistStamped, twist_cb)
-    rabbit = RabbitMotor()
+    rospy.Subscriber('/rex_velocity_controller/cmd_vel_out', TwistStamped, twist_cb)
+    rabbit = RexMotor()
 
     rospy.spin()
     IO.cleanup()
