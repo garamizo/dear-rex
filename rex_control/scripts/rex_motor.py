0d53b91853577ee9560ca310944b08d93e458f69
Guilherme A. Ribeiro <garamizo@mtu.edu>
2018-05-28 01:29:57 -0400

    fix pin state after python program shutdown

--- rex_control/scripts/rex_motor.py
+++ rex_control/scripts/rex_motor.py
@@ -15,6 +15,7 @@ class RexMotor:
 
     def __init__(self):
 
+        IO.setwarnings(False)
         IO.setmode(IO.BOARD)
         IO.setup(self.PIN_ENA, IO.OUT)
         IO.setup(self.PIN_IN1, IO.OUT)
@@ -26,11 +27,7 @@ class RexMotor:
         self.en1 = IO.PWM(self.PIN_ENA, 100)
         self.en2 = IO.PWM(self.PIN_ENB, 100)
 
-        IO.output(self.PIN_IN1, IO.HIGH)
-        IO.output(self.PIN_IN2, IO.LOW)
-
     def write(self, ur, ul):
-        print("%.2f %.2f" % (ur, ul))
         ur = np.clip(ur, -100, 100)
         ul = np.clip(ul, -100, 100)
 
@@ -46,14 +43,14 @@ class RexMotor:
 def twist_cb(msg):
     right = 100 * msg.twist.linear.x + 100 * msg.twist.angular.z
     left = 100 * msg.twist.linear.x - 100 * msg.twist.angular.z
-    rabbit.write(right, left)
+    rex.write(right, left)
 
 
 if __name__ == "__main__":
 
     rospy.init_node('simple_nav', anonymous=True)
     rospy.Subscriber('/rex_velocity_controller/cmd_vel_out', TwistStamped, twist_cb)
-    rabbit = RexMotor()
+    rex = RexMotor()
 
     rospy.spin()
-    IO.cleanup()
+    rex.write(0, 0)
